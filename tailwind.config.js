41f8015a0b9d752552710f0b0463ef236fb71765
akashpattnaik.github <akashpattnaik.github@gmail.com>
2024-03-21 16:11:47 +0000

    🚀 init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bayon: ["Bayon"],
+        andika: ["Andika"],
+        b612: ['B612Mono']
+      }
+    },
+  },
+  plugins: [],
+}
+
